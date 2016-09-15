9b5f03c84e0596d1d3a25bef3c561c3d9a2e4a42
Kendra Egert <mskendrae@gmail.com>
2016-09-14 21:15:47 -0400

    last commit

---
+++ SumIntegersHomework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumIntegersHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] addNum = { 1, 5, 7, 9 }; //create array
+            answer(addNum); 
+            Console.WriteLine("The sum of the AddNum array is {0}", answer(addNum)); //sum of adding array values
+        }
+
+        static int answer(params int[] numbers) //begin method
+        {
+            int sum = 0; // creating temp vaiable for math
+            
+            for (int i = 0; i < numbers.Length; i++) //counter to go thru the array
+            {
+                sum += numbers[i]; //math on the array variables
+            }
+            return sum; // sending final result to the writeline in main
+        }
+
+
+    }
+
+}
